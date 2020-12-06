57f80197c6b50d41cb829efcf47bc90275030792
jahin44 <jahinhasan1@gmail.com>
2020-12-06 23:05:04 +0600

    Devskills ans :(

---
+++ 4randomFileName/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ans4_random_string
+{
+    class Program
+    {
+
+        static Random random = new Random();
+        public static string RandomString()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_0123456789";
+            
+            var sb = new StringBuilder();
+            Random ran = new Random();
+            for (var i = 0; i < 16; i++)
+            {
+                var c = chars[ran.Next(0, 60)];
+                sb.Append(c);
+            }
+           return (sb.ToString());
+        }
+
+        static void Main(string[] args)
+        {
+           string name= RandomString();
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ 6 oddnumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OddDigitsfile
+{
+    class Program
+    {
+        public static List<int> OddDigits()
+        {
+            string inputnumber = Console.ReadLine();
+            List<int> output = new List<int>();            
+            for (int i = 0; i < inputnumber.Length; i++)
+            {
+                if ((Convert.ToInt32(inputnumber[i]) % 2) != 0)
+                { int a = Convert.ToInt32(inputnumber[i]);
+                    output.Add(a);
+                }
+                                   
+            }
+
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            List<int> num = new List<int>();
+           
+              num = OddDigits();
+            foreach(int data in num)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
